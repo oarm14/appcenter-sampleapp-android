684c459fd9878a778b288ad07405f9f0799542b0
oarm14 <59032199+oarm14@users.noreply.github.com>
2020-12-02 13:14:37 -0500

    Update appcenter-post-build.sh
    pull in scanner, run scanner to sonarcloud

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
 echo "This is an App Center Post-Build script. For more information on how to use App Center build scripts vist: https://docs.microsoft.com/en-us/appcenter/build/custom/scripts"
+
+export SONAR_SCANNER_VERSION=4.4.0.2170
+export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-macosx
+curl --create-dirs -sSLo $HOME/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-macosx.zip
+unzip -o $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/
+export PATH=$SONAR_SCANNER_HOME/bin:$PATH
+export SONAR_SCANNER_OPTS="-server"
+
+sonar-scanner \
+  -Dsonar.organization=oarm14 \
+  -Dsonar.projectKey=oarm14_appcenter-sampleapp-android \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=https://sonarcloud.io
